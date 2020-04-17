32b5a817339e63070355328132ba5c462c84baf0
Prome1heus <ginpa2000@gmail.com>
2020-04-18 01:06:08 +0200

    early working api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,28 @@
 from . import db 
+from sqlalchemy.orm import relationship
+from sqlalchemy import Table, Column, Integer, ForeignKey
 
-class Movie(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50))
-    rating = db.Column(db.Integer)
+    user_name = db.Column(db.String(50))
+    address = db.Column(db.String(50))
+
+class Shop(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	shop_name = db.Column(db.String(50))
+	address = db.Column(db.String(50))
+	sells = db.Column(db.String(50))
+
+class Product(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	shop_id = db.Column(db.Integer, ForeignKey('shop.id'))
+	product_name = db.Column(db.String(50))
+	quantity = db.Column(db.Integer)
+
+class Request(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	shop_id = db.Column(db.Integer, ForeignKey('shop.id'))
+	user_id = db.Column(db.Integer, ForeignKey('user.id'))
+	product_id = db.Column(db.Integer, ForeignKey('product.id'))
+	quantity = db.Column(db.Integer)
+	
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,90 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, request
 from . import db 
-from .models import Movie
+from .models import User, Product, Shop, Request
+from sqlalchemy import join
 
 main = Blueprint('main', __name__)
 
-@main.route('/add_movie', methods=['POST'])
-def add_movie():
-    movie_data = request.get_json()
+@main.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        user_list = User.query.all()
+        users = []
 
-    new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+        for user in user_list:
+            users.append({'user_name' : user.user_name, 'rating' : user.address})
 
-    db.session.add(new_movie)
-    db.session.commit()
+        return jsonify({'users' : users})
+    else:
+        user_data = request.get_json()
 
-    return 'Done', 201
+        new_user = User(user_name=user_data['user_name'], address=user_data['address'])
 
-@main.route('/movies')
-def movies():
-    movie_list = Movie.query.all()
-    movies = []
+        db.session.add(new_user)
+        db.session.commit()
 
-    for movie in movie_list:
-        movies.append({'title' : movie.title, 'rating' : movie.rating})
+        return 'Done', 201
 
-    return jsonify({'movies' : movies})
+@main.route('/shops', methods=['GET', 'POST'])
+def shops():
+    if request.method == 'GET':
+        shop_list = Shop.query.all()
+        shops = []
+
+        for shop in shop_list:
+            shops.append({'shop_name' : shop.shop_name, 'address' : shop.address, 'sells': shop.sells, 'id': shop.id})
+
+        return jsonify({'shops' : shops})
+    else:
+        shop_data = request.get_json()
+
+        new_shop = Shop(shop_name=shop_data['shop_name'], address=shop_data['address'], sells=shop_data['sells'])
+
+        db.session.add(new_shop)
+        db.session.commit()
+
+        return 'Done', 201
+
+@main.route('/requests', methods=['GET', 'POST'])
+def requests():
+    if request.method == 'GET':
+        request_list = db.session.query(Shop, User, Product, Request).filter(Shop.id == Request.shop_id).filter(User.id == Request.user_id).filter(Product.id == Request.user_id)
+        requests = []
+
+        for re in request_list:
+            requests.append({'shop_id': re.Shop.id, 'shop_name': re.Shop.shop_name, 'shop_address': re.Shop.address, 'user_id': re.User.id, 'user_name': re.User.user_name, 'user_address': re.User.address, 'product_id': re.Product.id, 'product_name': re.Product.product_name,'product_quantity': re.Product.quantity, 'request_quantity': re.Request.quantity})
+        
+        return jsonify({'requests' : requests})
+
+    else:
+        request_data = request.get_json()
+
+        new_request = Request(shop_id=request_data['shop_id'], user_id=request_data['user_id'], product_id=request_data['product_id'], quantity=request_data['quantity'])
+
+        db.session.add(new_request)
+        db.session.commit()
+
+        return 'Done', 201
+
+@main.route('/products', methods=['GET', 'POST'])
+def products():
+    if request.method == 'GET':
+
+        product_list =  db.session.query(Shop, Product).filter(Shop.id == Product.shop_id)
+        products = []
+
+        for product in product_list:
+            products.append({'product_id': product.Product.id,  'product_name': product.Product.product_name, 'quantity': product.Product.quantity, 'shop_id': product.Shop.id, 'shop_name': product.Shop.shop_name, 'addres': product.Shop.address, 'sells': product.Shop.sells})
+       
+        return jsonify({'products' : products})
+    else:
+        product_data = request.get_json()
+
+        new_product = Product(shop_id=product_data['shop_id'], product_name=product_data['product_name'], quantity=product_data['quantity'])
+
+        db.session.add(new_product)
+        db.session.commit()
+
+        return 'Done', 201
+
+    
