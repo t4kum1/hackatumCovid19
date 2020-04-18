05ea288a9ba96f98761875e2cc159c629e16e766
ge36zol <wenqing99.wei@tum.de>
2020-04-19 00:36:48 +0200

    corrected some more stuff

--- app/src/pages/components/Sidebar/Sidebar.js
+++ app/src/pages/components/Sidebar/Sidebar.js
@@ -8,8 +8,8 @@ const sidebar = props => {
     */
     let barClasses = 'sidebar';
     let clientBusinesslist = <ul>
-                                <Link to='/home'>Buy</Link>
-                                <Link to='/shoppingcart'>Shopping cart</Link>
+                                <Link to='/home'>Buy</Link><br></br>
+                                <Link to='/shoppingcart'>Shopping Cart</Link><br></br>
                             </ul>
     if(props.show) {
         barClasses= 'sidebar open';
@@ -17,10 +17,10 @@ const sidebar = props => {
 
     if(props.business) {
         clientBusinesslist = <ul>
-                                <Link to='/home'>Buy</Link>
-                                <Link to='/shoppingcart'>Shopping cart</Link>
-                                <Link to='/requestlist'>Requests</Link>
-                                <Link to='/stocklist'>Stock</Link>
+                                <Link to='/home'>Buy</Link><br></br>
+                                <Link to='/shoppingcart'>Shopping Cart</Link><br></br>
+                                <Link to='/requestlist'>Requests</Link><br></br>
+                                <Link to='/stocklist'>Stock</Link><br></br>
                             </ul>    
     }
 
